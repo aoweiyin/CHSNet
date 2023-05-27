4ea50a5771858bea829ec13d2254546808147d49
aoweiyin <2570568646@qq.com>
2023-05-27 22:12:02 +0800

    loss-change

--- losses/losses.py
+++ losses/losses.py
@@ -3,6 +3,19 @@ import torch.nn as nn
 from einops import rearrange
 
 
+# class DownMSELoss(nn.Module):
+#     def __init__(self, size=8):
+#         super().__init__()
+#         self.avgpooling = nn.AvgPool2d(kernel_size=size)
+#         self.tot = size * size
+#         self.mse = nn.MSELoss(reduction='sum')
+
+#     def forward(self, dmap, gt_density):
+#         gt_density = self.avgpooling(gt_density) * self.tot
+#         b, c, h, w = dmap.size()
+#         assert gt_density.size() == dmap.size()
+#         return self.mse(dmap, gt_density)
+
 class DownMSELoss(nn.Module):
     def __init__(self, size=8):
         super().__init__()
@@ -14,4 +27,8 @@ class DownMSELoss(nn.Module):
         gt_density = self.avgpooling(gt_density) * self.tot
         b, c, h, w = dmap.size()
         assert gt_density.size() == dmap.size()
-        return self.mse(dmap, gt_density)
+        res = dmap.clone()
+        res[gt_density==0] = 0
+        res_loss = self.mse(res, gt_density)
+        tot_loss = self.mse(dmap, gt_density)
+        return res_loss + 0.5 * tot_loss
